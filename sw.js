a290cde8da567280486f9bbd0ad0d6055523f0e4
bwana12 <onganimwase1@gmail.com>
2026-01-24 23:17:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,50 @@
 const CACHE_NAME = "shopeasy-v1";
-const urlsToCache = [
+
+const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
+  "./logo.png",
   "./manifest.json",
-  "./logo.png"
+
+  // Firebase SDKs
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js",
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js",
+
+  // Icons (add if you have them)
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll(ASSETS_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
+// Activate
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
-    )
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
   );
   self.clients.claim();
 });
 
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        return caches.match("./index.html");
+      });
     })
   );
 });
